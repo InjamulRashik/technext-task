82d54dd9182a25b42042569dd2c216804d195968
InjamulRashik <ihrashik@gmail.com>
2021-10-08 10:15:53 +0600

    Action And Reducer Created!

---
+++ src/components/DataContainer.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function DataContainer() {
+  return <div>Data</div>;
+}
+
+export default DataContainer;
---
+++ src/redux/actions/dataAction.js
@@ -0,0 +1,25 @@
+import {
+  FETCH_DATA_FAILURE,
+  FETCH_DATA_REQUEST,
+  FETCH_DATA_SUCCESS,
+} from "../constants/types";
+
+export const fetchDataRequest = () => {
+  return {
+    type: FETCH_DATA_REQUEST,
+  };
+};
+
+export const fetchDataSuccess = (data) => {
+  return {
+    type: FETCH_DATA_SUCCESS,
+    payload: data,
+  };
+};
+
+export const fetchDataFailure = (error) => {
+  return {
+    type: FETCH_DATA_FAILURE,
+    payload: error,
+  };
+};
