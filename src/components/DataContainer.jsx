8e06ecf7f22316f97f2ac2cbd75cf19eb07ed38c
InjamulRashik <ihrashik@gmail.com>
2021-10-08 10:38:01 +0600

    Connected!

--- src/components/DataContainer.jsx
+++ src/components/DataContainer.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import { connect } from "react-redux";
+import { fetchData } from "../redux/actions/dataAction";
 
 function DataContainer() {
   return <div>Data</div>;
 }
 
-export default DataContainer;
+const mapStateToProps = (state) => {
+  return {
+    data: state.data,
+  };
+};
+const mapDispatchToProps = (dispatch) => {
+  return {
+    fetchData: () => dispatch(fetchData()),
+  };
+};
+export default connect(mapStateToProps, mapDispatchToProps)(DataContainer);
--- src/redux/actions/dataAction.js
+++ src/redux/actions/dataAction.js
@@ -1,3 +1,4 @@
+import axios from "axios";
 import {
   FETCH_DATA_FAILURE,
   FETCH_DATA_REQUEST,
@@ -21,5 +22,21 @@ export const fetchDataFailure = (error) => {
   return {
     type: FETCH_DATA_FAILURE,
     payload: error,
+  };
+};
+
+export const fetchData = () => {
+  return (dispatch) => {
+    dispatch(fetchDataRequest);
+    axios
+      .get("https://api.spacexdata.com/v3/launches")
+      .then((response) => {
+        const data = response.data;
+        dispatch(fetchDataSuccess(data));
+      })
+      .catch((error) => {
+        const erroMessage = error.message;
+        dispatch(fetchDataFailure(erroMessage));
+      });
   };
 };
